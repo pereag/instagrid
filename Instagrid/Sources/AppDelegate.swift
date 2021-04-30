f0a901ad02a950b8c046fe1e72ddbfe9309131f5
valc0d3 <perellovalentinpro@gmail.com>
2021-04-30 11:05:00 +0200

    mobile integration ( no responsive)

---
+++ Instagrid/Sources/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Instagrid
+//
+//  Created by Valc0d3 on 23/04/2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
---
+++ Instagrid/Sources/homeViewController.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewController.swift
+//  Instagrid
+//
+//  Created by Valc0d3 on 23/04/2021.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+}
