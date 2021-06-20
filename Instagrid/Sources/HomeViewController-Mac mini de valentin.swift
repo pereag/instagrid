061a8554e405eaf7ed5aa1d1872eb7839fc35c0c
valc0d3 <perellovalentinpro@gmail.com>
2021-06-20 14:20:30 +0200

    push template image to library Ok

--- Instagrid/Sources/HomeViewController-Mac mini de valentin.swift
+++ Instagrid/Sources/HomeViewController-Mac mini de valentin.swift
@@ -8,9 +8,9 @@
 import UIKit
 
 final class HomeViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
+
     // MARK: - IBOutlets
-    
+
     @IBOutlet private weak var viewTitleLabel: UILabel!
     @IBOutlet private weak var swipeLabel: UILabel!
     @IBOutlet private weak var swipeArrowImageview: UIImageView!
@@ -29,6 +29,7 @@ final class HomeViewController: UIViewController, UIImagePickerControllerDelegat
     @IBOutlet weak var bottomLeftButton: UIButton!
     @IBOutlet weak var bottomRightButton: UIButton!
     
+    
     @IBOutlet weak var gridContainerView: UIView!
     
     // MARK: - Properties
@@ -39,14 +40,14 @@ final class HomeViewController: UIViewController, UIImagePickerControllerDelegat
         imagePicker.allowsEditing = true
         return imagePicker
     }()
-    
+
     var currentSelectedSpot: UIView? = nil
     var currentSelectedButton: UIButton? = nil
-    var currentSelectedGridFormatButton: UIButton? = nil
-    
+
     // MARK: - View life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        
     }
     
     // MARK: - IBActions
@@ -56,66 +57,53 @@ final class HomeViewController: UIViewController, UIImagePickerControllerDelegat
         layoutOneButton.alpha = 0.4
         layoutTwoButton.alpha = 1
         layoutThreeButton.alpha = 1
-        currentSelectedGridFormatButton = layoutOneButton
-        currentSelectedButton = layoutOneButton
     }
-    
     @IBAction private func didPressLayoutTwo() {
         bottomRightView.isHidden = true
         topRightView.isHidden = false
         layoutOneButton.alpha = 1
         layoutTwoButton.alpha = 0.4
         layoutThreeButton.alpha = 1
-        currentSelectedGridFormatButton = layoutTwoButton
-        currentSelectedButton = layoutTwoButton
     }
-    
     @IBAction private func didPressLayoutTree() {
         bottomRightView.isHidden = false
         topRightView.isHidden = false
         layoutOneButton.alpha = 1
         layoutTwoButton.alpha = 1
         layoutThreeButton.alpha = 0.4
-        currentSelectedGridFormatButton = layoutThreeButton
-        currentSelectedButton = layoutThreeButton
     }
-    
     @IBAction func didPressTopLeftButton() {
         openPhotoLibrary()
         currentSelectedSpot = topLeftView
         currentSelectedButton = topLeftButton
         
     }
-    
     @IBAction func didPressTopRightButton() {
         openPhotoLibrary()
         currentSelectedSpot = topRightView
         currentSelectedButton = topRightButton
         
     }
-    
     @IBAction func didPressBottomLeftButton() {
         openPhotoLibrary()
         currentSelectedSpot = bottomLeftView
         currentSelectedButton = bottomLeftButton
         
     }
-    
     @IBAction func didPressBottomRightButton() {
         openPhotoLibrary()
         currentSelectedSpot = bottomRightView
         currentSelectedButton = bottomRightButton
         
     }
-    
     @IBAction private func gestureReconizerSwipeUp(_ sender: UIPanGestureRecognizer) {
         switch sender.state {
-        case .began, .changed:
-            transformPositionGridContainerView(gesture: sender, screenVerticaly: isVerticaly())
-        case .ended, .cancelled:
-            resetPositionGridContainerView()
-        default:
-            break
+            case .began, .changed:
+                transformPositionGridContainerView(gesture: sender)
+            case .ended, .cancelled:
+                resetPositionGridContainerView()
+            default:
+                break
         }
     }
     
@@ -125,12 +113,12 @@ final class HomeViewController: UIViewController, UIImagePickerControllerDelegat
             self.present(imagePicker, animated: true, completion: nil)
         }
     }
-    
+
     // MARK: - Function UIImagePickerControllerDelegate
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let image = info[.originalImage] as? UIImage else { return }
         let imageView = UIImageView(image: image)
@@ -138,7 +126,7 @@ final class HomeViewController: UIViewController, UIImagePickerControllerDelegat
         buttonDidPressAlpha(btn: currentSelectedButton!)
         picker.dismiss(animated: true, completion: nil)
     }
-    
+
     func set(_ subView: UIView, on parentView: UIView?) {
         guard let _parentView = parentView, let lastView = _parentView.subviews.last else { return }
         _parentView.cleanAllSubviewsExceptLast()
@@ -157,79 +145,18 @@ final class HomeViewController: UIViewController, UIImagePickerControllerDelegat
     }
     
     // MARK: - Function gridContainerView
-    fileprivate func displayAlert() {
-        if VerifyIfGridIsCompletForScreenShotFormat() {
-            UIGraphicsBeginImageContext(gridContainerView.bounds.size)
-            guard let context = UIGraphicsGetCurrentContext() else {
-                UIGraphicsEndImageContext()
-                return
-            }
-            gridContainerView.layer.render(in: context)
-            guard let image = UIGraphicsGetImageFromCurrentImageContext() else {
-                UIGraphicsEndImageContext()
-                return
-            }
-            UIGraphicsEndImageContext()
-
-            let activityController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-            self.present(activityController, animated: true, completion: nil)
-        } else {
-            let alert = UIAlertController(title: "Erreur", message: "Le template photo n'est pas complet.", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Fermer", style: .default, handler: nil))
-            self.present(alert, animated: true)
-        }
-    }
-    
-    private func VerifyIfGridIsCompletForScreenShotFormat() -> Bool {
-        if currentSelectedGridFormatButton != nil {
-            if currentSelectedGridFormatButton! == layoutOneButton {
-                if topLeftView.subviews.count > 1 && bottomLeftView.subviews.count > 1 && bottomRightView.subviews.count > 1 {
-                    return true
-                } else {
-                    return false
-                }
-            } else if currentSelectedGridFormatButton! == layoutThreeButton {
-                if topLeftView.subviews.count > 1 && topRightView.subviews.count > 1 && bottomLeftView.subviews.count > 1 && bottomRightView.subviews.count > 1 {
-                    return true
-                } else {
-                    return false
-                }
-            } else {
-                if topLeftView.subviews.count > 1 && topRightView.subviews.count > 1 && bottomLeftView.subviews.count > 1 {
-                    return true
-                } else {
-                    return false
-                }
-            }
-        } else {
-            if topLeftView.subviews.count > 1 && topRightView.subviews.count > 1 && bottomLeftView.subviews.count > 1 {
-                return true
-            } else {
-                return false
-            }
-        }
-    }
-    
-    private func transformPositionGridContainerView(gesture: UIPanGestureRecognizer, screenVerticaly: Bool = false) {
+    private func transformPositionGridContainerView(gesture: UIPanGestureRecognizer) {
         let translation = gesture.translation(in: gridContainerView)
-        if screenVerticaly {
-            var translationYPositon = 0;
-            if translation.y <= 0 {
-                translationYPositon = Int(translation.y)
-            }
-            gridContainerView.transform = CGAffineTransform(translationX: 0, y: CGFloat(translationYPositon))
-            if translationYPositon <= -80 {
-                displayAlert ()
-            }
-        } else {
-            var translationXPositon = 0;
-            if translation.x <= 0 {
-                translationXPositon = Int(translation.x)
-            }
-            gridContainerView.transform = CGAffineTransform(translationX: CGFloat(translationXPositon), y:0)
-            if translationXPositon <= -80 {
-                displayAlert()
-            }
+        var translationYPositon = 0;
+        if translation.y <= 0 {
+            translationYPositon = Int(translation.y)
+        }
+        gridContainerView.transform = CGAffineTransform(translationX: 0, y: CGFloat(translationYPositon))
+        if translationYPositon <= -80 {
+            print("envoyer photo")
+            let alert = UIAlertController(title: "Alerte", message: "Montage incomplet.", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Fermer", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
         }
     }
     
@@ -238,24 +165,28 @@ final class HomeViewController: UIViewController, UIImagePickerControllerDelegat
     }
     
     // MARK: - Orientation detection
-    func isVerticaly() -> Bool {
+    override func didRotate(from fromInterfaceOrientation: UIInterfaceOrientation) {
+        var text=""
         switch UIDevice.current.orientation{
         case .portrait:
-            return true
+            text="Portrait"
         case .portraitUpsideDown:
-            return false
+            text="PortraitUpsideDown"
         case .landscapeLeft:
-            return false
+            text="LandscapeLeft"
         case .landscapeRight:
-            return false
+            text="LandscapeRight"
+            
         default:
-            return true
+            text="Another"
         }
+        NSLog("You have moved: \(text)")
     }
 }
 
 extension UIView {
     func cleanAllSubviewsExceptLast() {
+        print(self.subviews.count)
         var nbr = 0
         while nbr <= self.subviews.count - 2 {
             self.subviews[nbr].removeFromSuperview()
